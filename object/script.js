e145c177ee84f9c256c2885a123380f1f9d0d7ca
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-10-06 02:32:15 +0700

    object.create()

--- object/script.js
+++ object/script.js
@@ -20,17 +20,31 @@
 // };
 
 // Function declaration
-// function Mahasiswa(nama, energi) {
-//   let mahasiswa = {};
-//   mahasiswa.nama = nama;
-//   mahasiswa.energi = energi;
-//   mahasiswa.makan = function (porsi) {
-//     this.energi += porsi;
-//     console.log(`Haloo ${this.nama}, Selamat makan`);
-//   };
 
-//   return mahasiswa;
-// }
+const methodMahasiswa = {
+  makan: function (porsi) {
+    this.energi += porsi;
+    console.log(`Haloo ${this.nama}, Selamat makan`);
+  },
+  main: function (jam) {
+    this.energi -= jam;
+    console.log(`Haloo ${this.nama}, Selamat Main`);
+  },
+  tidur: function (jam) {
+    this.energi += jam * 2;
+    console.log(`Haloo ${this.nama} Selamat Tidur`);
+  },
+};
+
+function Mahasiswa(nama, energi) {
+  let mahasiswa = Object.create(methodMahasiswa);
+  mahasiswa.nama = nama;
+  mahasiswa.energi = energi;
+
+  return mahasiswa;
+}
+const riski = Mahasiswa("riski", 10);
+const budi = Mahasiswa("budi", 10);
 
 // function Mahasiswa(nama, energi) {
 //   return {
@@ -43,16 +57,14 @@
 //   };
 // }
 
-// const riski = Mahasiswa("riski", 10);
+// // Constractor function
+// function Mahasiswa(nama, energi) {
+//   this.nama = nama;
+//   this.energi = energi;
+//   this.makan = function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Haloo ${this.nama}, Selamat makann`);
+//   };
+// }
 
-// Constractor function
-function Mahasiswa(nama, energi) {
-  this.nama = nama;
-  this.energi = energi;
-  this.makan = function (porsi) {
-    this.energi += porsi;
-    console.log(`Haloo ${this.nama}, Selamat makann`);
-  };
-}
-
-const riski = new Mahasiswa("riski", 20);
+// const riski = new Mahasiswa("riski", 20);
