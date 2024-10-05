6948195276718a310f772f9fb4bc7b16a7d695c2
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-10-06 03:48:16 +0700

    prototype

---
+++ object/protorype.js
@@ -0,0 +1,35 @@
+// PROTOTYPE
+// function Mahasiswa(nama, energi) {
+//   this.nama = nama;
+//   this.energi = energi;
+// }
+
+// Mahasiswa.prototype.makan = function (porsi) {
+//   this.energi += porsi;
+//   console.log(`Haloo ${this.nama} Selamat makan`);
+// };
+// Mahasiswa.prototype.main = function (jam) {
+//   this.energi -= jam;
+//   console.log(`Haloo ${this.nama} Selamat main`);
+// };
+
+// const riski = new Mahasiswa("riski", 20);
+
+// Jika menggunkana class
+
+class Mahasiswa {
+  constructor(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+  }
+  makan(porsi) {
+    this.energi += porsi;
+    console.log(`Haloo ${this.nama} Selamat makan`);
+  }
+  main(jam) {
+    this.energi -= jam;
+    console.log(`Haloo ${this.nama} Selamat main`);
+  }
+}
+
+const budi = new Mahasiswa("budi", 20);
