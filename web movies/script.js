6b00c0e5ddb3f4aab1f59bebd783c22e93ac083b
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2024-10-24 21:20:24 +0700

    web film dengan request API imdb dengan Fetch

--- web movies/script.js
+++ web movies/script.js
@@ -1,33 +1,74 @@
-$(".search-button").on("click", function () {
-  $.ajax({
-    url: "http://www.omdbapi.com/?apikey=6de01f2b&s=" + $(".input-film").val(),
-    success: (response) => {
-      const { Search: films } = response;
+// $(".search-button").on("click", function () {
+//   $.ajax({
+//     url: "http://www.omdbapi.com/?apikey=6de01f2b&s=" + $(".input-film").val(),
+//     success: (response) => {
+//       const { Search: films } = response;
+//       let cards = ``;
+//       console.log(films);
+//       films.forEach((film) => {
+//         cards += showFilms(film);
+//       });
+//       $(".movie-list").html(cards);
+//       $(".movie-detail-button").on("click", function () {
+//         $.ajax({
+//           url:
+//             "http://www.omdbapi.com/?apikey=6de01f2b&i=" +
+//             $(this).data("imdbid"),
+//           success: (details) => {
+//             const movieDetails = showDetailsFilm(details);
+//             $(".modal-detail-movie").html(movieDetails);
+//           },
+//           error: (e) => {
+//             console.log(e.responseText);
+//           },
+//         });
+//       });
+//     },
+//     error: (e) => {
+//       console.log(e.responseText);
+//     },
+//   });
+// });
+
+// Menggunakan vanilla javascript
+
+const searchBtn = document.querySelector(".search-button");
+searchBtn.addEventListener("click", function () {
+  const inputFilm = document.querySelector(".input-film");
+  console.log("mulai");
+
+  fetch("http://www.omdbapi.com/?apikey=6de01f2b&s=" + inputFilm.value)
+    .then((response) => response.json())
+    .then((data) => {
+      const dataFilm = data.Search;
       let cards = ``;
-      console.log(films);
-      films.forEach((film) => {
-        cards += showFilms(film);
-      });
-      $(".movie-list").html(cards);
-      $(".movie-detail-button").on("click", function () {
-        $.ajax({
-          url:
-            "http://www.omdbapi.com/?apikey=6de01f2b&i=" +
-            $(this).data("imdbid"),
-          success: (details) => {
-            const movieDetails = showDetailsFilm(details);
-            $(".modal-detail-movie").html(movieDetails);
-          },
-          error: (e) => {
-            console.log(e.responseText);
-          },
+      if (dataFilm != undefined) {
+        dataFilm.forEach((film) => (cards += showFilms(film)));
+        const movieList = document.querySelector(".movie-list");
+        movieList.innerHTML = cards;
+
+        // KETIKA TOMBOL SHOW DETAIL DI KLIK
+        const movieDetailButton = document.querySelectorAll(
+          ".movie-detail-button"
+        );
+        movieDetailButton.forEach((btn) => {
+          btn.addEventListener("click", function () {
+            const imdbid = this.dataset.imdbid;
+            fetch("http://www.omdbapi.com/?apikey=6de01f2b&i=" + imdbid)
+              .then((response) => response.json())
+              .then((data) => {
+                const movieDetails = showDetailsFilm(data);
+                const modalDetailMovie = document.querySelector(
+                  ".modal-detail-movie"
+                );
+                modalDetailMovie.innerHTML = movieDetails;
+              });
+          });
         });
-      });
-    },
-    error: (e) => {
-      console.log(e.responseText);
-    },
-  });
+      }
+    });
+
+  console.log("selesai");
 });
 
 function showFilms(film) {
